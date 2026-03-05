3d586c2327565c694329ed0b66beec0c3c316660
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-03-05 12:26:50 +0100

    feat: restructure applications

--- vclusters/destroy.sh
+++ vclusters/destroy.sh
@@ -5,4 +5,4 @@ rm -f vclusters/configs/cluster-*.conf
 rm -f vclusters/configs/cluster-*.zip
 
 echo "deleting configuration from the cluster-access pod..."
-kubectl -n bswe-cluster-access get pod -l  "app.kubernetes.io/name=cluster-access" -o jsonpath="{.items[0].metadata.name}" | xargs -I {} kubectl -n bswe-cluster-access exec {} -- sh -c "rm /usr/share/nginx/html/files/cluster-*.zip"
+kubectl -n vcluster-access get pod -l  "app.kubernetes.io/name=cluster-access" -o jsonpath="{.items[0].metadata.name}" | xargs -I {} kubectl -n vcluster-access exec {} -- sh -c "rm /usr/share/nginx/html/files/cluster-*.zip"
--- vclusters/playbook.sh
+++ vclusters/playbook.sh
@@ -6,7 +6,7 @@ ZIP_PASSWORD=${ZIP_PASSWORD:-"password"}
 # generate configuration for each cluster
 for cluster in ${CLUSTER_IDS}; do
     echo "connecting to cluster-${cluster}..."
-    vcluster connect cluster-${cluster} -n cluster-${cluster} --server=https://${cluster}.cluster.classroom.bswe.hochschule-burgenland.muehlbachler.xyz --service-account admin --cluster-role cluster-admin --insecure --print > vclusters/configs/cluster-${cluster}.conf
+    vcluster connect cluster-${cluster} -n cluster-${cluster} --server=https://${cluster}.cluster.classroom.hochschule-burgenland.muehlbachler.xyz --service-account admin --cluster-role cluster-admin --insecure --print > vclusters/configs/cluster-${cluster}.conf
     KUBECONFIG="vclusters/configs/cluster-${cluster}.conf" kubectl get pod -A
 
     echo "zipping configuration for cluster-${cluster}..."
@@ -14,8 +14,8 @@ for cluster in ${CLUSTER_IDS}; do
 done
 
 # copy configuration to the cluster-access pod
-CLUSTER_ACCESS_POD=$(kubectl -n bswe-cluster-access get pod -l "app.kubernetes.io/name=cluster-access" -o jsonpath="{.items[0].metadata.name}")
+CLUSTER_ACCESS_POD=$(kubectl -n vcluster-access get pod -l "app.kubernetes.io/name=cluster-access" -o jsonpath="{.items[0].metadata.name}")
 for cluster in ${CLUSTER_IDS}; do
     echo "copying configuration for cluster-${cluster}..."
-    kubectl -n bswe-cluster-access cp vclusters/configs/cluster-${cluster}.zip $CLUSTER_ACCESS_POD:/usr/share/nginx/html/files/
+    kubectl -n vcluster-access cp vclusters/configs/cluster-${cluster}.zip $CLUSTER_ACCESS_POD:/usr/share/nginx/html/files/
 done
